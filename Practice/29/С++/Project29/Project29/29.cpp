a67b664bb2e309d46ee33e4a866a7ea116dc617f
BivshiySergey <0svbivshiy3@mail.ru>
2020-12-24 17:39:27 +0300

    29 in work

--- Practice/29/С++/Project29/Project29/29.cpp
+++ Practice/29/С++/Project29/Project29/29.cpp
@@ -3,6 +3,7 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <ctime>
 using namespace std;
 struct Student {
 	map<string, int> examens;
@@ -19,4 +20,39 @@ struct Student {
 		gruppa = group;
 		examens = { {"Math",math},{"Phys",phys}, {"Hyst",hyst},{"Prog" , prog} };
 	}
+	ostream& operator << (ostream& out, const Student& student) {
+		
+	}
 };
+int students() {
+	srand(time(NULL));
+	vector<Student> Students = { Student("Munyaya", 1,3,4,4,5),Student("WMAT", 2,3,3,4,2),
+								Student("Maxim", 3,5,4,4,5),  Student("Andrey", 4,4,4,4,4),
+								Student("Sergey", 5,4,4,4,3), Student("Danya", 6,3,2,4,5),
+								Student("Jaba" , 7,4,3,4,5),  Student("Legushka", 7,5,4,5,3),
+								Student("Vlad", 2,4,3,2,5),   Student("Gribnik" , 5,5,4,3,3), };
+	auto varmiyu = [](Student& student) -> bool {
+		for (auto& sec : student.examens) {
+			if (sec.second != 2) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+
+	};
+	vector<Student> ryadovoy;
+	for (auto& student : Students) {
+		if (varmiyu(student)) {
+			ryadovoy.push_back(student);
+		}
+	}
+	if (ryadovoy.size() != 0) {
+		cout << ryadovoy << endl;
+		cout << "Expulsion" << endl << ryadovoy[rand() % ryadovoy.size()];
+	}
+	else {
+		cout << "Not Found";
+	}
+}
